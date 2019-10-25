9be1c6e0f6aa61d59b94c091871be760c82f6b28
Ian Collington <iancollington@gmail.com>
2019-10-25 12:33:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Static content
+
+Contains static images for the products that are served from a dedicated CDN.
---
+++ iac/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+aws cloudformation update-stack --stack-name 'serverlesspizza-static-content-pipeline' \
+	--template-body file://cfn_codepipeline.yml \
+	--region eu-west-1 \
+	--parameters ParameterKey=GitHubToken,ParameterValue=$AWS_GITHUB_TOKEN \
+		ParameterKey=ModuleName,ParameterValue=static-content \
+	--capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND \
+	--profile aws-serverlesspizza-devops
